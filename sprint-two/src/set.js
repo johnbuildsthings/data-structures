165fd010e72b460d3f1286fb28f1d9213916e54b
Hack Reactor Students <accounts@hackreactor.com>
2015-07-04 17:38:30 -0700

    deleted comments from set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -43,65 +43,6 @@ setPrototype.remove = function(item){
 };
 
 setPrototype.deepCompare = function(val1, val2) {
-  // debugger;
-  // if (Array.isArray(val1) && Array.isArray(val2)) {
-  //   if (val1.length === val2.length) {
-  //     _.each(val1, function(item, key){
-  //       if (item !== val2[key]) {
-  //         return false;
-  //       }
-  //     });
-  //     return true;
-  //   }
-  // }
-  // else if (typeof val1 === 'object' && typeof val1 !== null){
-  //   if(typeof val2 === 'object' && typeof val2 !== null){
-  //     // debugger;
-  //     _.each(val1, function(item, key){
-  //       if(item !== val2[key] || item === undefined){
-  //         // debugger;
-  //         return false;
-  //       }
-  //     });
-  //     return true;
-  //   }
-  // }
-  // else {
-  //   return val1 === val2;
-  // }
-
-	// returns boolean
-
-  // Other Implementation
-  // var innerDeepCompare = function (type1, value2) {
-  //   if (Array.isArray(value2)) {
-  //     if (val1.length === val2.length) {
-  //        _.each(val1, function(item, key){
-  //       if (item !== val2[key]) {
-  //         return false;
-  //       }
-  //     });
-  //     return true;
-  //   }
-  //   else if (typeof value2 === 'object' && typeof value2 !== null) {
-
-  //   }
-  //   else {
-
-  //   }
-  // };
-
-  // if (Array.isArray(val1)){
-
-  // }
-  // else if (typeof val1 === 'object' && typeof val1 !== null) {
-
-  // }
-  // else {
-
-  // }
-
-  // Third Implementation //////////////////////
   var someArray = _.some([val1, val2], function(item){
     return Array.isArray(item);
   });
